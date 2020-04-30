6ea0a925f21c44d2762d5f044bf554e9a53b7641
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-04-29 21:23:24 -0500

    Remove alpha code branding and remove unnecessary setup stuff
    Removed some setup stuffs that was necessary but was specific to raspberry pi. Will fix soon

--- Core/AlphaMC/wizard/installer.py
+++ Core/AlphaMC/wizard/installer.py
@@ -1,45 +1,13 @@
-#AlphaMc Installer
+#MCBES Installer
 #runs on first boot
 import os
 from shutil import copyfile
 
 print " "
-print "Welcome to AlphaMc Server Software! You are going to setup the server now!"
+print "Welcome to MCBES Proxy Server ! You're about to setup the server!"
 print " "
-install = True
-while install:
-    choice = raw_input("Please accept the license. You are able to download and share this codes but cannot use in your builds without giving credit to owner. Y/N: ")
-    if choice == "Y":
-        print "License agreed!"
-        install = False
-        print " "
-    else:
-        print 'Please accept licence'
-
-print "	"
 servername = raw_input("Please put in your mcpe server name: ")
 print'You picked', servername
 print "	"
-
-install = True
-while install:
-    skip_installer = raw_input('Do you want to skip the installer? Y/N ')
-    if skip_installer == 'Y':
-        install = False
-    else:
-        print 'You will setup the server now!'
-        print ' '
-        
-	
-os.chdir('/home/pi/Desktop/AlphaMc')
-if not os.path.exists('worlds'):
-    os.makedirs('worlds')
-if not os.path.exists('plugins'):
-    os.makedirs('plugins')
-if not os.path.exists('crashes'):
-    os.makedirs('crashes')
-if not os.path.exists('players'):
-    os.makedirs('players')
-if not os.path.isfile('config.py'):
-    copyfile('/home/pi/Desktop/AlphaMc/Core/AlphaMc/res/config.py', '/home/pi/Desktop/AlphaMc/config.py')
+#TODO save config files
 print 'Setup complete!'
