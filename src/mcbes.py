ff67bbd6170ecaff38ad4d733cc1ea7f3c06e2ab
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-04-30 18:13:54 -0500

    Dev (#8)
    * Update README.md (#6)
    
    * bugfix for some issues to do with logging
    
    * Tests should pass

--- src/mcbes.py
+++ src/mcbes.py
@@ -10,13 +10,13 @@ try:
     handler = ServerHandler(server, None)
     handler.sendOption("name", "MCCPP;MINECON;TestServer")
     server.logger.info("server started!")
-    server.logger.info("Checking if this is a test build...")
+    server.logger.debug("Checking if this is a test build...")
     if "TRAVIS" in os.environ:
         handler.shutdown()
         time.sleep(2)
         sys.exit(0)
     else:
-      server.logger.info("Not a test build! Enjoy your proxy!")
+      server.logger.debug("Not a test build! Enjoy your proxy!")
 except Exception as e:
     server.logger.critical("Failed to start! error:"+str(e))
     handler.shutdown()
