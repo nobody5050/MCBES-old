3421116ff3b0106f4eaf29ce70cd06199b32917c
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-04-30 08:40:56 -0500

    Create start.py

---
+++ src/start.py
@@ -0,0 +1,13 @@
+from . libs.pyraklib.server import PyRakLibServer
+from . libs.pyraklib.server import ServerHandler
+import time
+import sys
+
+print("Starting server!")
+try:
+    server = PyRakLibServer(19132)
+    handler = ServerHandler(server, None)
+except Exeption as e:
+    print("Failed to start server!")
+    print(" ")
+    print(e)
