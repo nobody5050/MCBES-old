76db78647f834460251656cbde954b9b659dba84
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-04-30 17:38:35 -0500

    Bugfix, and make the test exit if failed

--- src/mcbes.py
+++ src/mcbes.py
@@ -2,6 +2,7 @@ from pyraklib.server import PyRakLibServer
 from pyraklib.server import ServerHandler
 import time
 import sys
+import os
 
 try:
     server = PyRakLibServer(19132)
@@ -18,3 +19,6 @@ try:
       server.logger.info("Not a test build! Enjoy your proxy!")
 except Exception as e:
     server.logger.critical("Failed to start! error:"+str(e))
+    handler.shutdown()
+        time.sleep(2)
+        sys.exit(1)
