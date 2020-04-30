0504589260e65075fbbf0e684395c69fdd7f9c22
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-04-30 17:26:43 -0500

    bugfix

--- src/mcbes.py
+++ src/mcbes.py
@@ -3,9 +3,9 @@ from pyraklib.server import ServerHandler
 import time
 import sys
 
-server.logger.info("Starting Proxy")
 try:
     server = PyRakLibServer(19132)
+    server.logger.info("Starting Proxy")
     handler = ServerHandler(server, None)
     handler.sendOption("name", "MCCPP;MINECON;TestServer")
     server.logger.info("server started!")
