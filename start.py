1d903f353fd1e44eded604d0dc4673e1121e448d
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-04-29 21:32:49 -0500

    Woo python 3. Possibly. Probably not

--- start.py
+++ start.py
@@ -5,27 +5,27 @@ from shutil import copyfile
 if not os.path.isfile('config.py'):
     os.chdir('src/installer')
     os.system('python installer.py')
-print "MCBES proxy is starting! Please wait! Version 1 beta!"
+print ("MCBES proxy is starting! Please wait! Version 1 beta!")
  
 HOST = '0.0.0.0'
 PORT = 19132
  
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print 'Opening host on 0.0.0.0!'
+print ('Opening host on 0.0.0.0!')
  
 try:
     s.bind((HOST, PORT))
 except socket.error as msg:
-    print 'Server failed to start! Maybe another server is using the port?'
+    print ('Server failed to start! Maybe another server is using the port?')
     sys.exit()
      
-print 'Server started!'
+print ('Server started!')
 
  
 s.listen(10)
 
 while 1:
     conn, addr = s.accept()
-    print 'Connected with ' + addr[0] + ':' + str(addr[1])
+    print ('Connected with ' + addr[0] + ':' + str(addr[1]))
      
 s.close()
