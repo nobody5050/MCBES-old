be78d804d18ce5bf776bcd56c5148a6805a9db08
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-04-30 11:50:40 -0500

    Move installer, it’s still very much TODO but whatever

---
+++ src/installer.py
@@ -0,0 +1,14 @@
+#MCBES Installer
+#runs on first boot
+import os
+from shutil import copyfile
+
+print(" ")
+print("Welcome to MCBES Proxy Server ! You're about to setup the server!")
+print(" ")
+servername = input("Please put in your mcpe server name: ")
+print('You picked', servername)
+print("	")
+#TODO save config files
+print('Setup complete!')
+#THIS FILE IS A REMNANT OF WHERE I FORKED THIS FROM. EVENTUALLY IT WILL BE MERGED TO ANOTHER FILE.
