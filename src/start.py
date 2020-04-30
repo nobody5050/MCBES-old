aedfe3228d22fc28a1f6fe2eb66138bf31997fde
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-04-30 13:59:37 -0500

    Partial update to server starting.

--- src/start.py
+++ src/start.py
@@ -1,1 +1,13 @@
-#TODO
+from libs.pyraklib.server import PyRakLibServer
+from libs.pyraklib.server import ServerHandler
+import time
+import sys
+
+print("Starting proxy!")
+try:
+    server = PyRakLibServer(19132)
+    handler = ServerHandler(server, None)
+    handler.sendOption("name", "MCCPP;MINECON;TestServer")
+    print("server started!")
+except Exception as e:
+    server.logger.critical("Failed to start! error:"+str(e))
