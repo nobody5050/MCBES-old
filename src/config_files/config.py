f95337e713b536b3029b1290a69d552367e44100
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-05-01 22:57:12 -0500

    Kill me (#12)
    * config files!
    
    * Make Booleans not strings.

---
+++ src/config_files/config.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#AlphaMc Config File, updated for use with mcbes
+#Version Of Config: 2
+
+#                   SERVER
+#login message
+loginmessage = '@player joined the game'
+#logout message
+logoutmessage = '@player left the game'
+#plugins (future stuff - might let the player do mods)
+plugins = true
+#unsupported block prefrance
+wool_for_unsupported_block = true
+update_block_for_unsupported_block = false
