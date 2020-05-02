7091d6e70dac29d11fb53bdec6c34698803453e5
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-05-01 22:26:57 -0500

    config files!

---
+++ src/config_files/config.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#AlphaMc Config File, updated for use with mcbes
+#Version Of Config: 2
+#All Options Are <TRUE OR FALSE>
+
+#                   SERVER
+#login message
+loginmessage = '@player joined the game'
+#logout message
+logoutmessage = '@player left the game'
+#plugins (future stuff - might let the player do mods)
+plugins = 'true'
+#unsupported block prefrance
+wool_for_unsupported_block = 'true'
+update_block_for_unsupported_block = 'false'
