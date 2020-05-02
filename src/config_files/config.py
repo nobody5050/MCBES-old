09c463c80490c939bcb42adff7c94628624623c4
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-05-02 16:30:26 -0500

    Development work (#14)
    * Update mcbes.py
    
    * Update installer.py
    
    * Kill me (#12)
    
    * config files!
    
    * Make Booleans not strings.
    
    * Better Travis logic.
    
    * fix indentation
    
    * Make build status not error
    
    * make build status not error, pt 2
    
    * pt 3
    
    * pt 4
    
    * very simple setup file, much TODO
    
    * Add name attribute, make json a thing
    
    * Update config.py
    
    * let us test a thing
    
    * Fix json syntax
    
    * Messed up a bit of the syntax, no worries
    
    * SYNTAXXXXX
    
    * Capitalization
    
    * Update mcbes.py

--- src/config_files/config.py
+++ src/config_files/config.py
@@ -1,14 +1,20 @@
 # -*- coding: utf-8 -*-
 #AlphaMc Config File, updated for use with mcbes
 #Version Of Config: 2
+#                   IMPORTS
+import json
 
 #                   SERVER
 #login message
-loginmessage = '@player joined the game'
-#logout message
-logoutmessage = '@player left the game'
-#plugins (future stuff - might let the player do mods)
-plugins = true
-#unsupported block prefrance
-wool_for_unsupported_block = true
-update_block_for_unsupported_block = false
+server = {
+  "name":"My Mcpi Proxy!",
+  "loginmessage":"@player joined the game",
+  #logout message
+  "logoutmessage":"@player left the game",
+  #plugins (future stuff - might let the player do mods)
+  "plugins":True,
+  #unsupported block prefrance
+  "wool_for_unsupported_block":True,
+  "update_block_for_unsupported_block":False,
+ 
+}
--- src/mcbes.py
+++ src/mcbes.py
@@ -6,9 +6,12 @@
 
 from pyraklib.server import PyRakLibServer
 from pyraklib.server import ServerHandler
+import config_files.config as cfg
 import time
 import sys
 import os
+import json
+import logging as log
 
 if "TRAVIS" in os.environ:
     try:
@@ -25,14 +28,15 @@ if "TRAVIS" in os.environ:
         time.sleep(2)
         sys.exit(1)
 else:
-    try:
+    try: 
+        name = json.loads(cfg.server.name)
         server = PyRakLibServer(19132)
-        server.logger.info("Starting Proxy")
+        log.info("Starting MCBES proxy! version 1.0")
         handler = ServerHandler(server, None)
         handler.sendOption("name", "MCCPP;MINECON;TestServer")
-        server.logger.info("server started!")
+        log.info("server started!")
     except Exception as e:
-        server.logger.critical("Failed to start! error:"+str(e))
+        log.critical("Failed to start! error:"+str(e))
         handler.shutdown()
         time.sleep(2)
         sys.exit(1)
