a7ad888deb18128c1d6e4bcf31f453b42e37b503
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2020-05-01 22:20:19 -0500

    Update installer.py

--- src/installer.py
+++ src/installer.py
@@ -1,5 +1,12 @@
+# this file is part of the mcbes project
+#   _  _   ___  ____  ____      ____ 
+#  ( \/ ) / __)(  _ \(  __)___ / ___)
+#  / \/ \( (__  ) _ ( ) _)(___)\___ \
+#  \_)(_/ \___)(____/(____)    (____/
+
 #MCBES Installer
 #runs on first boot
+
 import os
 from shutil import copyfile
 
